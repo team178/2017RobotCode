30d595f089bbc5ff88c9c1f7bb91ff86fb9e52bf
FishOfPitt116 <20fishelro@fpsct.org>
2017-01-23 19:14:53 -0500

    Work was done on the DriveTrain class

--- src/org/usfirst/frc/team178/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team178/robot/subsystems/DriveTrain.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team178.robot.subsystems;
 
-public class DriveTrain {
+public class DriveTrain extends Subsystem {
+	 
+	CANtalon left1;
+	CANtalon left2;
+	CANtalon right1;
+	CANtalon right2;
 
-}
+	public DriveTrain (int c1, int c2, int c3, int c4) {
+		left1= new CANtalon(c1);
+		left2= new CANtalon(c2);
+		right1= new CANtalon(c3);
+		right2= new CANtalon(c4);
+		
+	}
+	}
