8276e143c64e2e4215b34937a31f63ddfb07c8c6
poojpooj <pswami1600@gmail.com>
2017-01-25 18:36:13 -0500

    Started OI class, didn't assign buttons to commands because we don't have commands

--- src/org/usfirst/frc/team178/robot/OI.java
+++ src/org/usfirst/frc/team178/robot/OI.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team178.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team178.robot.commands.ExampleCommand;
 
@@ -9,6 +11,27 @@ import org.usfirst.frc.team178.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	
+	Joystick Andrew = new Joystick(RobotMap.JoystickPort); //I am sorry. jk best decision. 
+	Button button1 = new JoystickButton(Andrew, 1);
+	Button button2 = new JoystickButton(Andrew, 2);
+	Button button3 = new JoystickButton(Andrew, 3);
+	Button button4 = new JoystickButton(Andrew, 4);
+	Button button5 = new JoystickButton(Andrew, 5);
+	Button button6 = new JoystickButton(Andrew, 6);
+	Button button7 = new JoystickButton(Andrew, 7);
+	
+	Joystick Prashant = new Joystick(RobotMap.JoystickPortXbox);
+	Button buttonA = new JoystickButton(Prashant, 1);
+	Button buttonX = new JoystickButton(Prashant, 3);
+	Button buttonY = new JoystickButton(Prashant, 4);
+	Button lBumper = new JoystickButton(Prashant, 6);
+	Button rBumper = new JoystickButton(Prashant, 5);
+	Button buttonB = new JoystickButton(Prashant, 2);
+	Button backButton = new JoystickButton(Prashant, 7);
+	Button startButton = new JoystickButton(Prashant, 8); 
+ 
+	
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
