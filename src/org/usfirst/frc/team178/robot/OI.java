efb8f4b7a966317fee1ca589d0e7d1c24fdd4963
Elizabeth Cho <zergling13rush@gmail.com>
2017-01-27 18:24:47 -0500

    Cleaned up unneeded classes/functions

--- src/org/usfirst/frc/team178/robot/OI.java
+++ src/org/usfirst/frc/team178/robot/OI.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
-import org.usfirst.frc.team178.robot.commands.ExampleCommand;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
