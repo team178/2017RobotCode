ade479ee9e2350de18bcc5e8a96127e6bb386320
poojpooj <pswami1600@gmail.com>
2017-01-27 20:06:06 -0500

    fixed spacing and import errors in FuelShooter subsystem

--- src/org/usfirst/frc/team178/robot/subsystems/FuelShooter.java
+++ src/org/usfirst/frc/team178/robot/subsystems/FuelShooter.java
@@ -1,19 +1,19 @@
 package org.usfirst.frc.team178.robot.subsystems;
 
-import org.usfirst.frc.team178.robot.subsystems.CANTalon;
-import org.usfirst.frc.team178.robot.subsystems.Encoder;
-
+import com.ctre.CANTalon;
+import org.usfirst.frc.team178.robot.RobotMap;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class FuelShooter extends Subsystem {
+	
 	public static CANTalon shooter1;
 	public static CANTalon shooter2;
 	public static CANTalon shooter3;
 	public static Encoder eshooter;
-	
 	
 	public FuelShooter() {
 	
@@ -23,17 +23,15 @@ public class FuelShooter extends Subsystem {
 	eshooter = new Encoder(RobotMap.ENCODERshooter1, RobotMap.ENCODERshooter2);
 	
 	}
+	
 	public void shoot(double speed1, double speed2, double speed3) {
 		shooter1.set(speed1);
 		shooter2.set(speed2);
 		shooter3.set(speed3);
-		
-		
 	}
 	
 	public double getEncoderValue() {
 		return eshooter.getRate();
-		
 	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
