f668530db2d9e80097eea819794f06d7bc9ce4ec
Aditya <20ManikandanAd@fpsct.org>
2017-01-30 16:46:57 -0500

    Fixed the FuelShooter SubSystem

--- src/org/usfirst/frc/team178/robot/subsystems/FuelShooter.java
+++ src/org/usfirst/frc/team178/robot/subsystems/FuelShooter.java
@@ -12,14 +12,12 @@ public class FuelShooter extends Subsystem {
 	
 	public static CANTalon shooter1;
 	public static CANTalon shooter2;
-	public static CANTalon shooter3;
 	public static Encoder encoder;
 	
 	public FuelShooter() {
 	
 	shooter1 = new CANTalon(RobotMap.Shooter1);
 	shooter2 = new CANTalon(RobotMap.Shooter2);
-	shooter3 = new CANTalon(RobotMap.Shooter3);
 	encoder = new Encoder(RobotMap.SHOOTERencoderA, RobotMap.SHOOTERencoderB);
 	
 	}
@@ -28,7 +26,6 @@ public class FuelShooter extends Subsystem {
 	public void shoot(double shootSpeed, double intakeSpeed) {
 		shooter1.set(shootSpeed);
 		shooter2.set(shootSpeed);
-		shooter3.set(intakeSpeed);
 	}
 	
 	public void stop() {
