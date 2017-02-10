c2297ae84cf7ffe811fd314f51e5b3b6bc469070
Elizabeth Cho <zergling13rush@gmail.com>
2017-02-10 17:27:09 -0500

    Added empty commandgroups for remaining autonomous options
    Needs further work based off of other commands created

---
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team178.robot.autocommandgroups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoGearSequence extends CommandGroup {
+
+    public AutoGearSequence() {
+        //General sequence for autonomous gears, may need variations
+    	//Center: Drive forward, center, place gear
+    	//Left or right: Drive forward, rotate, center, place gear 
+    }
+}
---
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/AutoShootSequence.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team178.robot.autocommandgroups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoShootSequence extends CommandGroup {
+
+    public AutoShootSequence() {
+       //General sequence based off of positioning
+       //Drive forward, turn, aim
+    }
+}
