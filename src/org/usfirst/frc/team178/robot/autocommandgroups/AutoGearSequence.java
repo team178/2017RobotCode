1b3e030bc6f248ddb93b1ec2264b7c5a6bc3c894
poojpooj <pswami1600@gmail.com>
2017-02-10 18:27:32 -0500

    Started integration of vision code with autonomous commands

--- src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team178.robot.autocommandgroups;
 
+import org.usfirst.frc.team178.robot.commands.*;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,6 +10,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoGearSequence extends CommandGroup {
 
     public AutoGearSequence() {
+    	//Center alignment
+    	if()
+    	addSequential(new CenterOnAirship());
+    	addSequential(new MoveGobbler());
         //General sequence for autonomous gears, may need variations
     	//Center: Drive forward, center, place gear
     	//Left or right: Drive forward, rotate, center, place gear 
