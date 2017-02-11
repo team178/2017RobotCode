2b5a69cf2946f49146affdd1244d6c069ab30a20
Elizabeth Cho <zergling13rush@gmail.com>
2017-02-11 15:05:46 -0500

    Added approximations of distance values for AutoGearSequence
    sketchy sketchy math

--- src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
@@ -13,26 +13,26 @@ public class AutoGearSequence extends CommandGroup {
     public AutoGearSequence() {
     	DriverStation ds = DriverStation.getInstance();
     	
-    	if(ds.getLocation()==1) {
-    		addSequential(new DriveDistance(0.7), 2); //Change speed after testing
+    	if(ds.getLocation() == 1) { //It's either 1 or 3
+    		addSequential(new DriveDistance(0.5, 112), 2); //Change speed after testing
     		addSequential(new CenterOnAirship()); 
     		addSequential(new MoveGobbler());
     	}
     	//Left alignment
     	else if(ds.getLocation() == 2){
     		addSequential(new AutoTurn(0, 0.5), 0.5); //Adjust values after testing
-    		addSequential(new DriveDistance(0.7), 2); 
+    		addSequential(new DriveDistance(0.5, 231), 2); //Really sketchy math done
     		addSequential(new AutoTurn(0.5, 0), 1);
-    		addSequential(new DriveDistance(0.5), 1);
+    		addSequential(new DriveDistance(0.5, 111), 1); //231 * 0.48 (guessing)
     		addSequential(new CenterOnAirship());
     		addSequential(new MoveGobbler());
     	}
     	//Right alignment
-    	else{
+    	else {
     		addSequential(new AutoTurn(0.5, 0), 0.5);
-    		addSequential(new DriveDistance(0.7), 2);
+    		addSequential(new DriveDistance(0.5, 231), 2);
     		addSequential(new AutoTurn(0, 0.5), 1);
-    		addSequential(new DriveDistance(0.5), 1);
+    		addSequential(new DriveDistance(0.5, 111), 1);
     		addSequential(new CenterOnAirship());
     		addSequential(new MoveGobbler());
     	}
