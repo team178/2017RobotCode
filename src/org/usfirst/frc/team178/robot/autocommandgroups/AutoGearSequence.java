f92aa90d61a7e1ca98ae840e8f4151151afc6e4d
Elizabeth Cho <zergling13rush@gmail.com>
2017-02-11 17:40:34 -0500

    Switched left and center alignment

--- src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/AutoGearSequence.java
@@ -13,18 +13,19 @@ public class AutoGearSequence extends CommandGroup {
     public AutoGearSequence() {
     	DriverStation ds = DriverStation.getInstance();
     	
-    	if(ds.getLocation() == 1) { //It's either 1 or 3
-    		addSequential(new DriveDistance(0.5, 112)); //Change speed after testing
-    		addSequential(new CenterOnAirship()); 
-    		addSequential(new MoveGobbler());
-    	}
     	//Left alignment
-    	else if(ds.getLocation() == 2){
+    	if(ds.getLocation() == 1) { //It's either 1 or 3
     		addSequential(new AutoTurn(0, 0.5)); //Adjust values after testing
     		addSequential(new DriveDistance(0.5, 231)); 
     		addSequential(new AutoTurn(0.5, 0));
     		addSequential(new DriveDistance(0.5, 111));
     		addSequential(new CenterOnAirship());
+    		addSequential(new MoveGobbler());
+    	}
+    	//Center alignment
+    	else if(ds.getLocation() == 2){
+    		addSequential(new DriveDistance(0.5, 112)); //Change speed after testing
+    		addSequential(new CenterOnAirship()); 
     		addSequential(new MoveGobbler());
     	} 
     	//Right alignment
