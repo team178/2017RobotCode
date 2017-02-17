3188434e8b4ac6ee6ecfd8255bfeb564a5481605
Ronit Banerjee <ronitbandy@outlook.com>
2017-02-17 17:01:08 -0500

    Adjusted ClimbRope speed.

--- src/org/usfirst/frc/team178/robot/commands/ClimbRope.java
+++ src/org/usfirst/frc/team178/robot/commands/ClimbRope.java
@@ -24,7 +24,7 @@ public class ClimbRope extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	ropeclimber.climb(0);
+    	ropeclimber.climb(0.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
