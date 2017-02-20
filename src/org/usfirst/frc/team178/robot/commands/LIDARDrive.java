2a4061971828d4fc6237e8b9c5d809d71c97753f
FishOfPitt116 <20fishelro@fpsct.org>
2017-02-20 14:25:35 -0500

    Completed LIDARDrive command

--- src/org/usfirst/frc/team178/robot/commands/LIDARDrive.java
+++ src/org/usfirst/frc/team178/robot/commands/LIDARDrive.java
@@ -16,6 +16,7 @@ public class LIDARDrive extends Command {
 		LIDAR lidar;
 		double distance;
 		double robotSpeed;
+		double adjustedSpeed;
 		
 
     public LIDARDrive(double dist, double speed) {
@@ -32,18 +33,34 @@ public class LIDARDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	lidar.start();
     	drivetrain= Robot.drivetrain;
+    	lidar.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	drivetrain.drive(robotSpeed, -robotSpeed);
+    	double error = drivetrain.getLeftSpeed() - drivetrain.getRightSpeed(); //subtracts to find error value
+    	if (Math.abs(error) > 0.001)	{ //checks to see if the error value is greater than .001
+    		 //drives only left side
+    	    adjustedSpeed = 0.0001*error; //changes adjustedSpeed to adjustedSpeed + a constant*error
+        	double speedChange = (distance-drivetrain.getRightDistance())/distance;
+        	if (lidar.getDistance() >= (distance*.5))
+        	{
+        		drivetrain.drive(robotSpeed*speedChange, adjustedSpeed*speedChange);
+        	}
+        	else
+        	{
+        		drivetrain.leftDrive(robotSpeed);
+        		drivetrain.rightDrive(adjustedSpeed);//sets right side to adjustedSpeed
+
+        	}
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	if(1 == (distance - lidar.getDistance()*0.39)){
+    	if(1 >= (distance - lidar.getDistance()*0.39)) {
     		return true;
     	} else {
     		return false;
@@ -52,11 +69,14 @@ public class LIDARDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	drivetrain.drive(0,0);
     	lidar.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	drivetrain.drive(0, 0);
+    	lidar.stop();
     }
 }
