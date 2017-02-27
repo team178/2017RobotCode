ff591bd227fe0915cf49c15c9c3fc2af61d94f68
poojpooj <pswami1600@gmail.com>
2017-02-27 18:19:08 -0500

    Corrected CenterOnBoiler for driving rather than turning

--- src/org/usfirst/frc/team178/robot/commands/CenterOnBoiler.java
+++ src/org/usfirst/frc/team178/robot/commands/CenterOnBoiler.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team178.robot.commands;
 
+import org.usfirst.frc.team178.robot.Robot;
+import org.usfirst.frc.team178.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team178.robot.subsystems.VisionStreamer;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,30 +11,61 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class CenterOnBoiler extends Command {
 
+	private final Object imgLock = new Object();
+	DriveTrain drivetrain;
+	VisionStreamer camera;
+	double drive;
+	int threshold;
+	
     public CenterOnBoiler() {
+    	requires(Robot.drivetrain);
+		drivetrain = Robot.drivetrain;
+		threshold = 70;
+		camera = Robot.shooterCamera;
+		requires(Robot.shooterCamera);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	drive = -.1;
+		drivetrain.changeToLoGear();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double error = camera.getCenterYfromCameraCenterY();
+		if ((Math.abs(error) > threshold) && (Math.abs(error) < 350)) {
+			drive = -.0015 * error;
+			drivetrain.drive(drive/2, -drive/2);
+			drivetrain.correctSpeed();
+		}else if(Math.abs(error) > 300){
+		System.out.println("still not really sure what this does, but hey, if it works");
+		}else{
+			drivetrain.drive(0, 0);
+		}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if (Math.abs(camera.getCenterXfromCameraCenterX()) < threshold )  {
+		System.out.println("THRESHOLD END: " + ((camera.getBlendedCenterY()) - (camera.getIMG_HEIGHT() / 2)));
+		drivetrain.drive(0, 0);
+		return true;
+	} else {
+		return false;
+		}
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	drivetrain.drive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	drivetrain.drive(0, 0);
     }
 }
